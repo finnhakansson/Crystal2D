b72ba1e47c5452f00599a45eec53fdcc0b4a8b1d
Finn Hakansson <finn.hakansson@experian.com>
2012-08-21 17:18:15 -0400

    Some initial tests.

---
+++ Crystal2D.java
@@ -0,0 +1,35 @@
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+public class Crystal2D {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Crystal 2D");
+		Crystal2D c = new Crystal2D();
+		frame.getContentPane().add(c.new Crystal("Bajs"));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+	}
+
+	class Crystal extends JComponent {
+		private static final long serialVersionUID = 1L;
+		public Crystal(String x) {
+		}
+		public void paintComponent(Graphics g) {
+			g.drawString("APA", 100, 100);
+		}
+	}
+
+	class Model {
+		public Model() {
+			
+		}
+	}
+}
