c142a56f358a15634f5521a819769147c972ce39
Finn Hakansson <finn_hakansson@yahoo.com>
2012-09-28 08:56:09 -0400

    Updates.

--- Crystal2D.java
+++ Crystal2D.java
@@ -9,25 +9,41 @@ public class Crystal2D {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Crystal 2D");
 		Crystal2D c = new Crystal2D();
-		frame.getContentPane().add(c.new CrystalPane("Bajs"));
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(300, 300);
-		frame.setVisible(true);
 		Crystal crystal = c.new Crystal(300, 300);
 		crystal.initCrystalOneAtomMiddle();
+		CrystalPane cp = c.new CrystalPane("Bajs");
+		cp.setMatrix(crystal.getMatrix());
+		frame.getContentPane().add(cp);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 300 + 22);
+		frame.setVisible(true);
 		int x = 0;
 		int y = 0;
-		// while loop here.
+		//while () {
+		//}
 	}
 
 	class CrystalPane extends JComponent {
 		private static final long serialVersionUID = 1L;
+		private boolean[][] matrix;
 
 		public CrystalPane(String x) {
+		}
+		
+		public void setMatrix(boolean[][] matrix) {
+			this.matrix = matrix;
 		}
 
 		public void paintComponent(Graphics g) {
 			//g.drawString("APA", 100, 100);
+			g.drawArc(0, 0, 299, 299, 0, 360);
+			for (int y = 0; y < 300; y++) {
+				for (int x = 0; x < 300; x++) {
+					if (this.matrix[y][x]) {
+						g.drawLine(x, y, x, y);
+					}
+				}
+			}
 		}
 	}
 
@@ -78,7 +94,7 @@ public class Crystal2D {
 			for (int y = yBegin; y <= yEnd; y++) {
 				for (int x = xBegin; x <= xEnd; x++) {
 					if (this.matrix[y][x]) {
-						this.matrix[yLocation][xLocation] = 1;
+						this.matrix[yLocation][xLocation] = true;
 						return true;
 					}
 				}
@@ -86,12 +102,15 @@ public class Crystal2D {
 			return false;
 		}
 
-		boolean insideCircle(int x, int y) {
+		public boolean insideCircle(int x, int y) {
 			// From http://stackoverflow.com/questions/481144/equation-for-testing-if-a-point-is-inside-a-circle
 			// (x-center_x)^2 + (y - center_y)^2 < radius^2
 			return true;
 		}
 
+		public boolean[][] getMatrix() {
+			return this.matrix;
+		}
 	}
 }
 
